f32684baefb6dfb2f97697a920ca999b1fd62259
Ryan Patel <rajanpatel1011@gmail.com>
2018-03-28 16:31:14 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -16,5 +16,5 @@ function divSelector(){
   return $('div.pics,div.pics').addClass('pics');
 }
 function firstListItem(){
-  return $('ul#pic-list').childern('li').html('');
+  return $('ul#pic-list').childern('li').get();
 }
